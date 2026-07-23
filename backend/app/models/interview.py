56e20d9822676a26147f523be96e2814ab743f17
MudassarGill <mudassarhussain6533@gmail.com>
2026-07-22 23:15:42 -0700

    updated news file

--- backend/app/models/interview.py
+++ backend/app/models/interview.py
@@ -26,7 +26,6 @@ from sqlalchemy import Column, String, Float, DateTime, ForeignKey, JSON
 from sqlalchemy.dialects.postgresql import UUID
 
 from app.db.database import Base
-import pandas as pd 
 
 
 class Interview(Base):
